3eabfdc2ad1ec9b37f743732b2621312f4d839a5
amaryp <philippe.amary@ligue-cancer.net>
2024-12-11 15:27:50 +0100

    commit des modifs 11/12/2024

--- custom/questionnaire_pc/models/SurveyAnswerCronJob.py
+++ custom/questionnaire_pc/models/SurveyAnswerCronJob.py
@@ -31,6 +31,96 @@ class SurveyAnswerCronJob(models.Model):
                 email = email_line.value_char_box
                 _logger.info(f"Réponse soumise pour l'email : {email}")
 
+                # Recherche du nom dans les réponses, basé sur la question contenant 'nom'
+                # On accède explicitement à la valeur de "title" dans le champ JSON
+                ''' # a refaire une fois le nouveau champs reference implementé
+                
+                for question in user_input.survey_id.question_ids:
+                    title = question.title  # Récupérer le champ JSON title de la question
+                    if isinstance(title, dict):  # Vérifier si title est un dictionnaire
+                        # On vérifie si 'nom' est dans le libellé de la question
+                          title_fr = title.get('fr_FR', '')  # Récupérer la valeur en français (fr_FR)
+                          if 'nom' in title_fr.lower():  # Recherche insensible à la casse
+                            # Recherche de la réponse pour cette question
+                            name_line = self.env['survey.user_input.line'].search([
+                                ('user_input_id', '=', user_input.id),
+                                ('question_id', '=', question.id)
+                            ], limit=1)
+                            break
+                '''
+                #traite le nom
+                name_line = None
+                name_line = self.env['survey.user_input.line'].search([
+                ('user_input_id', '=', user_input.id),
+                ('question_id', '=', 4)  # ID de la question 'nom'
+                ], limit=1)
+
+                # Si une ligne de réponse est trouvée pour la question contenant "nom"
+                name = None
+                if name_line:
+                    name = name_line.value_char_box  # Récupère le nom de la réponse
+                    name = name.capitalize()  # Mettre la première lettre en majuscule
+                    _logger.info(f"Réponse soumise pour le nom : {name}")
+                #traite le prenom
+                firstname_line = None
+                firstname_line = self.env['survey.user_input.line'].search([
+                ('user_input_id', '=', user_input.id),
+                ('question_id', '=', 5)  # ID de la question 'prenom'
+                ], limit=1)
+
+                # Si une ligne de réponse est trouvée pour la question contenant "prenom"
+                firstname = None
+                if firstname_line:
+                    firstname = firstname_line.value_char_box  # Récupère le nom de la réponse
+                    firstname = firstname.capitalize()  # Mettre la première lettre en majuscule
+                    _logger.info(f"Réponse soumise pour le prenom : {firstname}")
+                #traite la date de naissance
+                dateofbirth_line = None
+                dateofbirth_line = self.env['survey.user_input.line'].search([
+                ('user_input_id', '=', user_input.id),
+                ('question_id', '=', 6)  # ID de la question 'date de naissance'
+                ], limit=1)
+
+                # Si une ligne de réponse est trouvée pour la question contenant "date de naissance"
+                dateofbirth = None
+                if dateofbirth_line:
+                    dateofbirth = dateofbirth_line.value_date  # Récupère le nom de la réponse
+                    _logger.info(f"Réponse soumise pour la date de naissance : {dateofbirth}")
+
+                #traite le telephone
+                telephone_line = None
+                telephone_line = self.env['survey.user_input.line'].search([
+                ('user_input_id', '=', user_input.id),
+                ('question_id', '=', 9)  # ID de la question 'telephone'
+                ], limit=1)
+
+                # Si une ligne de réponse est trouvée pour la question contenant "telephone"
+                telephone = None
+                if telephone_line:
+                    telephone = telephone_line.value_char_box  # Récupère le nom de la réponse
+                    _logger.info(f"Réponse soumise pour le telephone : {telephone}")
+
+                #traite le comité
+                comite_line = None
+                comite_line = self.env['survey.user_input.line'].search([
+                ('user_input_id', '=', user_input.id),
+                ('question_id', '=', 11)  # ID de la question 'comite'
+                ], limit=1)
+
+                comite = None
+                if comite_line:
+                 # Si une ligne de réponse est trouvée pour la question 'comité'
+                 # On accède à l'ID de la réponse suggérée via suggested_answer_id
+                    suggested_answer_id = comite_line.suggested_answer_id
+                    if suggested_answer_id:
+                        comite = suggested_answer_id.value  # Récupère la valeur de la réponse suggérée
+                        _logger.info(f"Réponse suggérée pour le comité : {comite}")
+                    else:
+                        _logger.warning(f"Aucune réponse suggérée trouvée pour la question 'comité'.")
+                else:
+                    _logger.warning(f"Aucune ligne de réponse trouvée pour la question 'comité'.")
+
+
                 # Création de la décision si l'email n'existe pas déjà dans pc.survey.decision
                 decision_model = self.env['pc.survey.decision']
                 existing_decision = decision_model.search([('email', '=', email)], limit=1)
@@ -40,6 +130,11 @@ class SurveyAnswerCronJob(models.Model):
                         'email': email,
                         #'statut': 'pending',
                         #'commentaire': '',
+                        'nom': name,
+                        'prenom':firstname,
+                        'date_naissance':dateofbirth,
+                        'telephone':telephone,
+                        'comite':comite,
                         'user_input_id':user_input.id
 
                     })
